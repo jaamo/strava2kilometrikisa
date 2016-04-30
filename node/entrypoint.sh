9b27a7a1dfafa3fa77e792b90ea53ef7109cbf22
Paul Stewart <paulstewart@Pauls-MacBook-Pro-2.local>
2016-04-30 13:52:57 +0300

    updated with mapped volumes, nodemon watching your changes

--- node/entrypoint.sh
+++ node/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-nodemon app.js
+#change dir to the app folder
+cd /srv/www/app
+
+#run nodemon so we can watch for changes in the app files, important note, we musy use --legacy-watch to allow files to be watched over the network of containers
+nodemon --legacy-watch app.js
