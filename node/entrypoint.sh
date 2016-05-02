825dfe7526020bc759eaf400775958c5775771ca
Paul Stewart <paulstewart@Pauls-MacBook-Pro-2.local>
2016-05-02 21:02:36 +0300

    changed node container to an slimmed down ubuntu image so we can run the node cron

--- node/entrypoint.sh
+++ node/entrypoint.sh
@@ -12,5 +12,14 @@ bower install --allow-root
 #build dist css and js
 gulp build
 
+#write out current crontab
+crontab -l > mycron
+#echo new cron into cron file
+echo "* 4 * * * node /srv/www/app/cron.js" >> mycron
+#install new cron file
+crontab mycron
+#remove tmp var
+rm mycron
+
 #run nodemon so we can watch for changes in the app files, important note, we musy use --legacy-watch to allow files to be watched over the network of containers
 nodemon --legacy-watch app.js
