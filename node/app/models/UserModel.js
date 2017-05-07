ca238b63ce503f41fd153b04c5fdba68741f48df
jaamo <jaakko@evermade.fi>
2017-05-07 16:14:14 +0300

    Added encrypt/decrypt method.

--- node/app/models/UserModel.js
+++ node/app/models/UserModel.js
@@ -1,4 +1,13 @@
 const mongoose = require('mongoose');
+var crypto = require('crypto');
+
+
+
+// Crypto algorithm.
+var algorithm = 'aes-256-ctr';
+var cryptoPassword = process.env.CRYPTO_PASSWORD;
+
+
 
 // Define schema for user.
 var UserSchema = new mongoose.Schema({
@@ -6,6 +15,8 @@ var UserSchema = new mongoose.Schema({
     stravaToken: { type: String },
     kilometrikisaToken: { type: String },
     kilometrikisaSessionId: { type: String },
+    kilometrikisaUsername: { type: String },
+    kilometrikisaPassword: { type: String },
     email: { type: String },
 
     // Sync kilometers automatically.
@@ -17,7 +28,35 @@ var UserSchema = new mongoose.Schema({
 
 });
 
+
+
+// Encrypt and set password.
+UserSchema.methods.setPassword = function(password) {
+
+    var cipher = crypto.createCipher(algorithm, cryptoPassword);
+    var crypted = cipher.update(password, 'utf8', 'hex');
+    crypted += cipher.final('hex');
+    this.kilometrikisaPassword = crypted;
+
+}
+
+
+
+// Decrypt and get password.
+UserSchema.methods.getPassword = function() {
+
+    var decipher = crypto.createDecipher(algorithm, cryptoPassword);
+    var dec = decipher.update(this.kilometrikisaPassword, 'hex', 'utf8');
+    dec += decipher.final('utf8');
+    return dec;
+
+}
+
+
+
 // Create model.
 var user = mongoose.model('User', UserSchema);
+
+
 
 module.exports = user;
